f0a6cf642a74fba86d354c4606360b424d7c6b45
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-06-22 14:30:40 +0530

    Added prompt for  showing  current directory.

--- main.c
+++ main.c
@@ -4,21 +4,34 @@ void set_color(Char_Ptr color_code){
   printf("\033%s", color_code);
 }
 
-int main(void){
-  int exit_code = 0;
+void __print_dir(){
+  char cwd[1024]; 
+  getcwd(cwd, sizeof(cwd)); 
+  printf("\nDir: %s\n", cwd); 
+}
 
-  while (1)
-  {
-    Char_Ptr color = GREEN;
-    char line[255];
+void __print_prompt(int exit_code){
+    Char_Ptr color = BOLD_CYAN;
     set_color(color);
-    printf("my-shell ");
+    __print_dir();
+    color = GREEN;
+    set_color(color);
+    printf("  my-shell ");
     if(exit_code) {
       color = RED;
     }
     set_color(color);
     printf("$  ");
     set_color(RESET);
+}
+
+int main(void){
+  int exit_code = 0;
+
+  while (1)
+  {
+    __print_prompt(exit_code);
+    char line[255];
     gets(line);
     Char_Ptr_To_Ptr command = parse_command(line);
     if(!handle_built_in(command)){
